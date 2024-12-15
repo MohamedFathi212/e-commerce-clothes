98717697a892d676c108ef98797ad44fb4c01303
MohamedFathi212 <mf974476@gmail.com>
2024-12-16 01:12:30 +0200

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -315,10 +315,10 @@
                             <a href="{{route('home.index')}}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="shop.html" class="navigation__link">Shop</a>
+                            <a href="{{route('shop.index')}}" class="navigation__link">Shop</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="cart.html" class="navigation__link">Cart</a>
+                            <a href="{{route('cart.index')}}" class="navigation__link">Cart</a>
                         </li>
                         <li class="navigation__item">
                             <a href="about.html" class="navigation__link">About</a>
@@ -398,16 +398,17 @@
                     </a>
                 </div>
 
+
                 <nav class="navigation">
                     <ul class="navigation__list list-unstyled d-flex">
                         <li class="navigation__item">
                             <a href="{{route('home.index')}}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="shop.html" class="navigation__link">Shop</a>
+                            <a href="{{route('shop.index')}}" class="navigation__link">Shop</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="cart.html" class="navigation__link">Cart</a>
+                            <a href="{{route('cart.index')}}" class="navigation__link">Cart</a>
                         </li>
                         <li class="navigation__item">
                             <a href="about.html" class="navigation__link">About</a>
@@ -417,6 +418,7 @@
                         </li>
                     </ul>
                 </nav>
+
 
                 <div class="header-tools d-flex align-items-center">
                     <div class="header-tools__item hover-container">
@@ -492,22 +494,20 @@
                             <use href="#icon_heart" />
                         </svg>
                     </a>
-
-                    <a href="cart.html" class="header-tools__item header-tools__cart">
+                    <a href="{{route('cart.index')}}" class="header-tools__item header-tools__cart">
                         <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <use href="#icon_cart" />
                         </svg>
-                        <span class="cart-amount d-block position-absolute js-cart-items-count">3</span>
+                        @if(Cart::instance('cart')->content()->count() > 0)
+                        <span class="cart-amount d-block position-absolute js-cart-items-count">{{Cart::instance('cart')->content()->count()}}</span>
+                        @endif
                     </a>
                 </div>
             </div>
         </div>
     </header>
-
     @yield('content')
-
-
     <hr class="mt-5 text-secondary" />
     <footer class="footer footer_type_2">
         <div class="footer-middle container">
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
@@ -22,6 +24,11 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/shop',[ShopController::class, 'index'])->name('shop.index');
+Route::get('/shop/{product_slug}',[ShopController::class, 'product_details'])->name('shop.product.details');
+
+Route::get('/cart',[CartController::class, 'index'])->name('cart.index');
+Route::post('/cart/add',[CartController::class, 'add_to_cart'])->name('cart.add');
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
