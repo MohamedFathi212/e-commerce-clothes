0ae3d62c70d1c46627bac421642e2234a3d84e9c
MohamedFathi212 <mf974476@gmail.com>
2025-01-15 21:44:21 +0200

    add

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -9,40 +11,59 @@ class ShopController extends Controller
 {
     public function index(Request $request)
     {
-        $size = $request->query('size') ? $request->query('size') : 12 ;
-        $o_column ="";
-        $o_order ="";
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_column = "";
+        $o_order = "";
         $order = $request->query('order') ? $request->query('order') : -1;
-        switch($order)
-        {
+        $f_brands = $request->query('brands');
+        $f_categories = $request->query('categories');
+
+        switch ($order) {
             case 1:
-                $o_column ='created_at';
+                $o_column = 'created_at';
                 $o_order = 'DESC';
                 break;
             case 2:
-                $o_column ='created_at';
+                $o_column = 'created_at';
                 $o_order = 'ASC';
                 break;
             case 3:
-                $o_column ='regular_price';
+                $o_column = 'regular_price';
                 $o_order = 'ASC';
                 break;
             case 4:
-                $o_column ='regular_price';
+                $o_column = 'regular_price';
                 $o_order = 'DESC';
                 break;
             default:
-                $o_column ='id';
+                $o_column = 'id';
                 $o_order = 'DESC';
         }
-        $products = Product::orderBy($o_column,$o_order)->paginate( $size);
-        return view('shop',compact('products', 'size','order'));
+
+        $brands = Brand::orderBy('name', 'ASC')->get();
+        $categories = Category::orderBy('name', 'ASC')->get();
+
+        $products = Product::where(function ($query) use ($f_brands) {
+                if ($f_brands) {
+                    $query->whereIn('brand_id', explode(',', $f_brands));
+                }
+            })
+            ->where(function ($query) use ($f_categories) {
+                if ($f_categories) {
+                    $query->whereIn('category_id', explode(',', $f_categories));
+                }
+            })
+            ->orderBy($o_column, $o_order)
+            ->paginate($size);
+
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories'));
     }
 
     public function product_details($product_slug)
     {
-        $product = Product::where('slug',$product_slug)->first();
-        $rproducts = Product::where('slug','<>',$product_slug)->get()->take(8);
-        return view('details', compact('product','rproducts'));
+        $product = Product::where('slug', $product_slug)->first();
+        $rproducts = Product::where('slug', '<>', $product_slug)->take(8)->get();
+
+        return view('details', compact('product', 'rproducts'));
     }
 }
